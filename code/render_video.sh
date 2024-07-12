7abd3400e3ad16b9f35dbbcfb5abb280cc69dd79
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2024-07-12 12:05:06 +0200

    Fix output video path, tags argument

--- code/render_video.sh
+++ code/render_video.sh
@@ -22,10 +22,10 @@ do
            -to "$end" \
            -c:v libsvtav1 -preset 6 -svtav1-params lp="$n_threads" \
            -c:a libopus -ac 1 -ab 24k \
-           "${date}_${track}_${name}.webm"
+           "${collection_dir}/${date}_${track}_${name}.webm"
 
     python3 code/create_xml.py "${collection_dir}/clips.tsv" "$clip_no" "/tmp/${date}_${track}_${name}.xml"
 
     mkvpropedit "${collection_dir}/${date}_${track}_${name}.webm" \
-                --tags "/tmp/${date}_${track}_${name}.xml"
+                --tags "all:/tmp/${date}_${track}_${name}.xml"
 done
